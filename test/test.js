d794de02ea36f5e30a78d6480af1600f9874213d
式燕 <wenjian.ywj@alibaba-inc.com>
2017-08-06 22:41:38 +0800

    update

---
+++ test/test.js
@@ -0,0 +1,51 @@
+describe('this', function () {
+  it('setTimeout', function (done) {
+    var obj = {
+      say: function () {
+        setTimeout(() => {
+          // this 是什么？想想为什么？
+          this.should.equal(null)
+          done()
+        }, 0)
+      }
+    }
+    obj.say()
+  }) 
+
+  it('global', function () {
+    function test() {
+      // this 是什么？想想为什么？
+      this.should.equal(null)
+    }
+    test()
+  })
+
+  describe('bind', function () {
+    it('bind undefined', function () {
+      var obj = {
+        say: function () {
+          function _say() {
+            // this 是什么？想想为什么？
+            this.should.equal(null)
+          }
+          return _say.bind(obj)
+        }()
+      }
+      obj.say()
+    })
+
+    it('bind normal', function () {
+      var obj = {}
+      obj.say = function () {
+        function _say() {
+          // this 是什么？想想为什么？
+          this.should.equal(null)
+        }
+        return _say.bind(obj)
+      }()
+      obj.say()
+    })
+  })
+
+  
+})
