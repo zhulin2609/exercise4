32c0bad5caa016327b1de71314e711e4e80a4760
zhuanyongxigua <zhuanyongxigua@gmail.com>
2018-06-19 13:24:16 +0800

    finish

--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 箭头函数中的this是上一级的this，这种情况下，谁调用的函数，this就是谁。
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,7 +16,8 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      // 相当于是使用global.test()的方式调用。
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +28,9 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            // 在对象赋值给obj之前自执行函数执行，这个时候已经过了预解释阶段，obj已经被声明，但却没有被赋值，是undefined，
+            // bind方法把当前的obj放到了内存里面，同闭包的情况一样，无法修改，找不到this的都是global。
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +43,8 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 在函数表达式赋值给obj.say之前自执行函数执行，这个时候obj已经被赋值成了{}，bind之后的情况与上例相同。需要注意的是虽然obj无法被重新赋值其他的数据，但是修改obj的属性是可以的。
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
